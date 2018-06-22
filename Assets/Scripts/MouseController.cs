40835d196561b01fe4eaca060485482b67b7e7c4
mikelHorbach <32166057+mikelHorbach@users.noreply.github.com>
2018-06-22 19:01:45 +0300

    Initial commit

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    public float jetpackForce = 75.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+
+        if (jetpackActive)
+        {
+            Rigidbody2D  body = this.GetComponent<Rigidbody2D>();
+            body.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
