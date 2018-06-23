fa55b812372b471f7194ac27a98034f110d2f49c
mikelHorbach <32166057+mikelHorbach@users.noreply.github.com>
2018-06-23 14:42:10 +0300

    animation & fixed flames

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -7,9 +7,16 @@ public class MouseController : MonoBehaviour {
     public float jetpackForce = 75.0f;
     public float forwardSpeed = 3.0f;
     private Rigidbody2D body;
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+    private Animator mouseAnimator;
+    public ParticleSystem jetpack;
+
     // Use this for initialization
     void Start () {
         body = this.GetComponent<Rigidbody2D>();
+        mouseAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -23,5 +30,27 @@ public class MouseController : MonoBehaviour {
         Vector2 newVelocity = body.velocity;
         newVelocity.x = forwardSpeed;
         body.velocity = newVelocity;
+        UpdateGroundedStatus();
+        AdjustJetpack(jetpackActive);
+    }
+
+    void UpdateGroundedStatus()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        mouseAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        var jetpackEmission = jetpack.emission;
+        jetpackEmission.enabled = !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackEmission.rateOverTime = 300.0f;
+        }
+        else
+        {
+            jetpackEmission.rateOverTime = 75.0f;
+        }
     }
 }
