1a31cf6c207fe0d02dec0e9f5834b6ffd8598595
mikelHorbach <32166057+mikelHorbach@users.noreply.github.com>
2018-06-23 11:38:41 +0300

    added an endless level

---
+++ Assets/Scripts/LayerFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "player";
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = -1;
+    }
+	
+}
--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class MouseController : MonoBehaviour {
 
     public float jetpackForce = 75.0f;
-
+    public float forwardSpeed = 3.0f;
+    private Rigidbody2D body;
     // Use this for initialization
     void Start () {
-		
-	}
+        body = this.GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
-
+        
         if (jetpackActive)
-        {
-            Rigidbody2D  body = this.GetComponent<Rigidbody2D>();
-            body.AddForce(new Vector2(0, jetpackForce));
+        {   
+            body.AddForce(new Vector2(0, jetpackForce)); 
         }
+        Vector2 newVelocity = body.velocity;
+        newVelocity.x = forwardSpeed;
+        body.velocity = newVelocity;
     }
 }
