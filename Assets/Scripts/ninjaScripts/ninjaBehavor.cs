025b09d0a844f306c6d9544eecb1fdb0cc7f8027
Alexeev-Andrew <andrey.alexeev.1998@gmail.com>
2018-06-25 22:34:34 +0300

    addNinjaAssets

---
+++ Assets/Scripts/ninjaScripts/ninjaBehavor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ninjaBehavor : MonoBehaviour {
+
+    public int speed = 1;
+
+    private Rigidbody2D body;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        body = this.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
