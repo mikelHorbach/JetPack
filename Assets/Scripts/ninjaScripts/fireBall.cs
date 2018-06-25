a35a3f1fdfecdfd7185145175976d48502bf4f29
Alexeev-Andrew <andrey.alexeev.1998@gmail.com>
2018-06-26 00:35:48 +0300

    Ninja and fireball prefabs

---
+++ Assets/Scripts/ninjaScripts/fireBall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBall : MonoBehaviour {
+
+    public float speed = 4;
+
+    void Start()
+    {
+        StartCoroutine(destroyLater());
+    }
+
+    IEnumerator destroyLater()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        MouseController mm = collider.GetComponent<MouseController>();
+        if (mm != null)
+        {
+            mm.Die();
+            Destroy(this.gameObject);
+        }
+           
+    }
+
+
+    public void launch()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2( -speed, 0);
+        GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+
+}
